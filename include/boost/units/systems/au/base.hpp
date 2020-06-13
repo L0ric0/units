a2ae20879e4c296b471fb538dce1a23c951f977b
Lorenz Steinert <lorenz@steinerts.de>
2020-06-13 19:27:29 +0200

    setup unit system

--- include/boost/units/systems/au/base.hpp
+++ include/boost/units/systems/au/base.hpp
@@ -17,38 +17,38 @@
 #include <boost/units/unit.hpp>
 #include <boost/units/make_system.hpp>
 
+#include <boost/units/base_units/au/bohr.hpp>
 #include <boost/units/base_units/au/boltzmann.hpp>
-#include <boost/units/base_units/au/charge.hpp>
-#include <boost/units/base_units/au/coulomb_force.hpp>
+#include <boost/units/base_units/au/candela.hpp>
+#include <boost/units/base_units/au/current.hpp>
 #include <boost/units/base_units/au/electron_mass.hpp>
-#include <boost/units/base_units/au/hbar.hpp>
-
-#include <boost/units/base_units/si/meter.hpp>
-
+#include <boost/units/base_units/au/mole.hpp>
+#include <boost/units/base_units/au/time.hpp>
+#include <boost/units/base_units/angle/radian.hpp>
+#include <boost/units/base_units/angle/steradian.hpp>
 
 namespace boost {
+    namespace units {
+        namespace au {
 
-namespace units { 
+            /// placeholder class defining au unit system
+            typedef make_system<
+                bohr_base_unit,
+                boltzmann_base_unit,
+                candela_base_unit,
+                current_base_unit,
+                electron_mass_base_unit,	
+                mole_base_unit,
+                time_base_unit,
+                angle::radian_base_unit,
+                angle::steradian_base_unit,
+            >::type system;
 
-namespace au {
+            /// dimensionless au unit
+            typedef unit<dimensionless_type,system> dimensionless;
 
-/// placeholder class defining au unit system
-typedef make_system<
-                    boltzmann_base_unit,
-                    charge_base_unit,
-		    coulomb_force_base_unit,
-//		boost::units::si::meter_base_unit,	
-                    electron_mass_base_unit,
-		    hbar_base_unit
-		    >::type system;
-
-/// various unit typedefs for convenience
-typedef unit<dimensionless_type,system>         dimensionless;
-
-} // namespace au
-                                                    
-} // namespace units
-
-} // namespace boost
+        }
+    }
+}
 
 #endif // BOOST_UNITS_AU_BASE_HPP
